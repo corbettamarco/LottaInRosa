1d957d7082b66d05f84bc93f29acf75e1fe6cf7f
Marco Corbetta <corbettamarco.it@gmail.com>
2024-04-21 21:25:13 +0200

    Update ThankYouPage.tsx

--- src/components/FormPages/ThankYouPage.tsx
+++ src/components/FormPages/ThankYouPage.tsx
@@ -28,7 +28,7 @@ export const ThankYouPage = ({
         <br />
       </Text>
       <Text color="black" fontSize="1.2rem" fontWeight={"bold"} px={".5rem"}>
-        {correctAnswers && correctAnswers <= 3 && (
+        {correctAnswers <= 3 && (
           <>
             Sembri saperne troppo poco, corri a informarti sul sito{" "}
             <a href="http://www.lottainrosa.it">www.lottainrosa.it</a> e scopri
